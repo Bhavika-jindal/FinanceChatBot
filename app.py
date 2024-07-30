32e3a5cacf08a5a96a9ad8a1ebb9cd84ec6a28ee
Bhavika-jindal <100370408+Bhavika-jindal@users.noreply.github.com>
2024-07-31 02:55:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ if user_input:
     try:
         st.session_state['messages'].append({'role':'user','content':user_input})
 
-        response = openai.FunctionsCompletion.create(
+        response = openai.chat.completions.create(
             model='gpt-4-0613',
             messages=st.session_state['messages'],
             functions=functions,
